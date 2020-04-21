0421c6feff7be506b127cc3c996802d78fd6a7ba
VishnuGettam <vishnugettam@gmail.com>
2020-04-21 16:35:40 +0530

    New User List added.

--- MVCAppFormsDemo/Controllers/UserController.cs
+++ MVCAppFormsDemo/Controllers/UserController.cs
@@ -1,17 +1,32 @@
-﻿using System;
+﻿using MVCAppFormsDemo.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace MVCAppFormsDemo.Controllers
 {
     public class UserController : Controller
     {
+        private List<User> _userName;
+
+        public UserController()
+        {
+            _userName = new List<User>()
+            {
+                new User(){ FirstName="Vishnu",LastName="Gettam",Id=1,Location="Chittoor" },
+                new User(){ FirstName="Vihaan",LastName="Gettam",Id=1,Location="Chittoor" },
+                new User(){ FirstName="Divya",LastName="Gettam",Id=1,Location="Chittoor" },
+                new User(){ FirstName="Lucky",LastName="Gettam",Id=1,Location="Chittoor" }
+            };
+        }
+
         // GET: User
         public ActionResult Index()
         {
-            return View();
+            return View(_userName);
         }
     }
 }
